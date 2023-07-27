c26bee91ef5c54f1b104169f2cee2a6a1a8f5b2f
Umut <mehmetumutmutlu@gmail.com>
2023-07-27 23:28:08 +0300

    The advanced prime app was updated.

--- advancedPrime/src/Main.java
+++ advancedPrime/src/Main.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    static boolean prime(int n) {
-        boolean result = true;
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                result = false;
-                break;
-            }
+    static void isPrime(int n) {
+        if (n < 1) {
+            System.out.println("Error: Invalid number.");
+            return;
         }
-        return result;
+        if (primeCheck(n, n - 1) == 0) {
+            System.out.println(n + " is NOT a prime number.");
+        } else {
+            System.out.println(n + " is a prime number.");
+        }
     }
-    static boolean prime2(int n) {
-        int i = n - 1;
-        return n % i != 0;
-        prime2((n - 1));
+    static int primeCheck(int n, int check) {
+        if (check == 1) {
+            return 1;
+        } else if (n % check == 0) {
+            return 0;
+        } else {
+            return primeCheck(n, check-1);
+        }
     }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter a number: ");
         int n = input.nextInt();
 
-        System.out.println(prime2(n));;
+        isPrime(n);
     }
 }
